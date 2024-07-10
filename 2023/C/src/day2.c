a324dae66760d890548b7ec14931f7f7ba460f45
nathandow <nathandow@gmail.com>
2024-07-10 09:32:45 -0700

    Removing unused code

--- 2023/C/src/day2.c
+++ 2023/C/src/day2.c
@@ -5,7 +5,6 @@
 #include <string.h>
 #include "inc/util.h"
 
-//int sumPlayableGames(FILE *fp, int maxR, int maxG, int maxB);
 void parseLine(char *buf, int len, int *gameId, int *r, int *g, int *b);
 
 int runDay2Part1(const char *file, int maxR, int maxG, int maxB) {
@@ -75,44 +74,3 @@ void parseLine(char *buf, int len, int *gameId, int *r, int *g, int *b) {
         }
     }
 }
-
-/*
-int sumPlayableGames(FILE *fp, int maxR, int maxG, int maxB) {
-    int sum = 0;
-    int lines = 0;
-    char buf[256];
-
-    while (fgets(buf, 256, fp) != NULL) {
-        int gameId = 0;
-        int num = 0;
-        int r = 0;
-        int g = 0;
-        int b = 0;
-
-        for (int i = 0; i < 256; i++) {
-            if (i == 0) {
-                i += strlen("Game ");
-                i += readNumber(&buf[i], &gameId);
-                i += strlen(": ");
-            }
-
-            if (isdigit(buf[i]) != 0) {
-                i += readNumber(&buf[i], &num);
-            } else {
-                if (buf[i] == '\n' || buf[i] == '\0') { break; }
-                else if (startsWith("red", &buf[i]) == 0) { r = max(r, num); }
-                else if (startsWith("green", &buf[i]) == 0) { g = max(g, num); }
-                else if (startsWith("blue", &buf[i]) == 0) { b = max(b, num); }
-            }
-        }
-        
-        if (r <= maxR && g <= maxG && b <= maxB) {
-            sum += gameId;
-        }
-
-        lines++;
-    }
-
-    return sum;
-}
-*/
