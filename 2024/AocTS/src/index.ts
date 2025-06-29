49c95efb92eb47ad9d3eade564a433883d3ac1ee
nathandow <nathandow@gmail.com>
2025-06-29 10:02:37 -0700

    General clean up of genereted files.

--- 2024/AocTS/src/index.ts
+++ 2024/AocTS/src/index.ts
@@ -1,31 +1,27 @@
-import * as FS from 'node:fs';
+import * as FS from "node:fs";
 
-runAll();
-
-function runAll() {
-  const start = performance.now();
-  day1part1();
-  day1part2();
-  day2part1();
-  day2part2();
-  day3part1();
-  day3part2();
-  day4part1();
-  day4part2();
-  day5part1();
-  const end = performance.now();
-  console.log(`total: ${(end - start).toFixed(2)}`);
-}
+day1part1();
+day1part2();
+day2part1();
+day2part2();
+day3part1();
+day3part2();
+day4part1();
+day4part2();
+day5part1();
+day5part2();
+day6part1();
+day6part2();
 
 // answer 2580760
 function day1part1() {
   const start = performance.now();
-  const data = FS.readFileSync('text/day1').toString();
+  const data = FS.readFileSync("text/day1").toString();
   var sum = 0;
 
   var left: number[] = [];
   var right: number[] = [];
-  data.split('\n').forEach((line) => {
+  data.split("\n").forEach((line) => {
     const numbers = line.split(/\s+/);
     if (numbers.length === 2) {
       left.push(parseInt(numbers[0]));
@@ -47,13 +43,13 @@ function day1part1() {
 // answer: 25358365
 function day1part2() {
   const start = performance.now();
-  const data = FS.readFileSync('text/day1').toString();
+  const data = FS.readFileSync("text/day1").toString();
   var score = 0;
 
   var left: Map<number, number> = new Map();
   var right: number[] = [];
 
-  data.split('\n').forEach((line) => {
+  data.split("\n").forEach((line) => {
     const numbers = line.split(/\s+/);
     if (numbers.length === 2) {
       left.set(parseInt(numbers[0]), 0);
@@ -78,10 +74,10 @@ function day1part2() {
 // answer: 213
 function day2part1() {
   const start = performance.now();
-  const data = FS.readFileSync('text/day2').toString();
+  const data = FS.readFileSync("text/day2").toString();
   var safeReports = 0;
 
-  data.split('\n').forEach((line) => {
+  data.split("\n").forEach((line) => {
     const report: number[] = line.split(/\s+/).map((s) => parseInt(s));
     if (isReportSafe(report)) ++safeReports;
   });
@@ -109,17 +105,18 @@ function inRange(x: number, min: number, max: number) {
 // answer: 285
 function day2part2() {
   const start = performance.now();
-  const data = FS.readFileSync('text/day2').toString();
+  const data = FS.readFileSync("text/day2").toString();
   var safeReports = 0;
 
-  data.split('\n').forEach((line) => {
+  data.split("\n").forEach((line) => {
     const report: number[] = line.split(/\s+/).map((s) => parseInt(s));
     if (isReportSafe(report)) {
       ++safeReports;
     } else {
       for (var i = 0; i < report.length; ++i) {
-        const modArray = report.toSpliced(i, 1);
-        if (isReportSafe(modArray)) {
+        var reportCopy = report.slice();
+        reportCopy.splice(i, 1);
+        if (isReportSafe(reportCopy)) {
           ++safeReports;
           break;
         }
@@ -134,7 +131,7 @@ function day2part2() {
 // answer: 192767529
 function day3part1() {
   const start = performance.now();
-  const data = FS.readFileSync('text/day3').toString();
+  const data = FS.readFileSync("text/day3").toString();
   var sum = 0;
 
   var regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
@@ -150,7 +147,7 @@ function day3part1() {
 // answer: 104083373
 function day3part2() {
   const start = performance.now();
-  const data = FS.readFileSync('text/day3').toString();
+  const data = FS.readFileSync("text/day3").toString();
   var sum = 0;
 
   var nextDont = 0;
@@ -165,7 +162,7 @@ function day3part2() {
       sum += parseInt(match[1]) * parseInt(match[2]);
     }
 
-    nextDo = data.indexOf('do()', nextDont);
+    nextDo = data.indexOf("do()", nextDont);
     if (nextDo === -1) break;
     if (nextDont === data.length - 1) break;
   }
@@ -177,7 +174,7 @@ function day3part2() {
 // answer: 2297
 function day4part1() {
   const start = performance.now();
-  const data = FS.readFileSync('text/day4').toString();
+  const data = FS.readFileSync("text/day4").toString();
   var count = 0;
 
   for (var y = 0; y < 140; ++y) {
@@ -229,7 +226,7 @@ function day4part1() {
 }
 
 function isXMAS(str: string, index: number, xDir: number, yDir: number) {
-  const xmasArray: string[] = ['X', 'M', 'A', 'S'];
+  const xmasArray: string[] = ["X", "M", "A", "S"];
   var arr: string[] = [];
 
   arr[0] = str[index];
@@ -246,7 +243,7 @@ function isXMAS(str: string, index: number, xDir: number, yDir: number) {
 // answer: 1745
 function day4part2() {
   const start = performance.now();
-  const data = FS.readFileSync('text/day4').toString();
+  const data = FS.readFileSync("text/day4").toString();
   var count = 0;
 
   for (var y = 0; y < 139; ++y) {
@@ -260,22 +257,22 @@ function day4part2() {
 }
 
 function isCrossMAS(str: string, index: number): boolean {
-  if (str[index] !== 'A') {
+  if (str[index] !== "A") {
     return false;
   }
 
   var leftToRight = false;
   if (
-    (str[index - 142] === 'M' && str[index + 142] === 'S') ||
-    (str[index - 142] === 'S' && str[index + 142] === 'M')
+    (str[index - 142] === "M" && str[index + 142] === "S") ||
+    (str[index - 142] === "S" && str[index + 142] === "M")
   ) {
     leftToRight = true;
   }
 
   var rightToLeft = false;
   if (
-    (str[index - 140] === 'M' && str[index + 140] === 'S') ||
-    (str[index - 140] === 'S' && str[index + 140] === 'M')
+    (str[index - 140] === "M" && str[index + 140] === "S") ||
+    (str[index - 140] === "S" && str[index + 140] === "M")
   ) {
     rightToLeft = true;
   }
@@ -283,14 +280,186 @@ function isCrossMAS(str: string, index: number): boolean {
   return leftToRight && rightToLeft;
 }
 
-// answer: TODO
+// answer: 4281
 function day5part1() {
   const start = performance.now();
-  const data = FS.readFileSync('text/day5').toString();
-  var count = 0;
+  const data = FS.readFileSync("text/day5").toString().split("\n\n");
+  const rules: Set<string> = new Set(data[0].split("\n"));
+  const updates: string[] = data[1].split("\n");
 
-  // TODO: implementation
+  function pageSort(left: string, right: string): number {
+    const str = left + "|" + right;
+    if (rules.has(str)) return -1;
+    if (rules.has(right + "|" + left)) return 1;
+    return 0;
+  }
+
+  let sum = 0;
+  for (const u of updates) {
+    let pages = u.split(",");
+    let sorted = pages.slice(0).sort(pageSort);
+
+    let correct = true;
+    for (let e = 0; e < pages.length; ++e) {
+      if (sorted[e] !== pages[e]) {
+        correct = false;
+        break;
+      }
+    }
+
+    if (correct) {
+      sum += parseInt(pages[Math.floor(pages.length / 2)]);
+    }
+  }
 
   const end = performance.now();
-  console.log(`4-2: ${count} in ${(end - start).toFixed(2)}`);
+  console.log(`5-1: ${sum} in ${(end - start).toFixed(2)}`);
+}
+
+// answer: 5466
+function day5part2() {
+  const start = performance.now();
+  const data = FS.readFileSync("text/day5").toString().split("\n\n");
+  const rules: Set<string> = new Set(data[0].split("\n"));
+  const updates: string[] = data[1].split("\n");
+
+  function pageSort(left: string, right: string): number {
+    const str = left + "|" + right;
+    if (rules.has(str)) return -1;
+    if (rules.has(right + "|" + left)) return 1;
+    return 0;
+  }
+
+  let sum = 0;
+  for (const u of updates) {
+    let pages = u.split(",");
+    let sorted = pages.slice(0).sort(pageSort);
+
+    let isSorted = false;
+    for (let e = 0; e < sorted.length; ++e) {
+      if (sorted[e] !== pages[e]) {
+        isSorted = true;
+      }
+    }
+
+    if (isSorted) {
+      sum += parseInt(sorted[Math.floor(sorted.length / 2)]);
+    }
+  }
+
+  const end = performance.now();
+  console.log(`5-2: ${sum} in ${(end - start).toFixed(2)}`);
+}
+
+// answer: 5199
+function day6part1() {
+  const LEN = 130;
+
+  function turnLookup(dir: string): string {
+    switch (dir) {
+      case "^":
+        return ">";
+      case ">":
+        return "v";
+      case "v":
+        return "<";
+      case "<":
+        return "^";
+    }
+
+    return "";
+  }
+
+  function backup(dir: string): number[] {
+    switch (dir) {
+      case "^":
+        return [1, 0];
+      case ">":
+        return [0, -1];
+      case "v":
+        return [-1, 0];
+      case "<":
+        return [0, 1];
+    }
+
+    return [0, 0];
+  }
+
+  function isGuard(c: string): boolean {
+    return c === "^" || c === "v" || c === "<" || c === ">";
+  }
+
+  function walk(dir: string, y: number, x: number): number {
+    //console.log(`walk: ${dir} (${y},${x})`);
+    let wdir = dir;
+    let wy = y;
+    let wx = x;
+    let wturns = 0;
+
+    while (true) {
+      switch (wdir) {
+        case "^":
+          --wy;
+          break;
+        case ">":
+          ++wx;
+          break;
+        case "v":
+          ++wy;
+          break;
+        case "<":
+          --wx;
+          break;
+      }
+
+      if (wy < 0 || wy === LEN || wx < 0 || wx === LEN) break;
+
+      //console.log(`    : ${dir} (${wy},${wx})`);
+      if (data[wy][wx] === "#") {
+        //console.log(`bonk: ${dir} (${wy},${wx})`);
+        const backCoords = backup(wdir);
+        wy += backCoords[0];
+        wx += backCoords[1];
+        wdir = turnLookup(wdir);
+        ++wturns;
+        const childTurns = walk(wdir, wy, wx);
+        wturns += childTurns;
+        return wturns;
+      } else {
+        nodes.add(`${wy}_${wx}`);
+      }
+    }
+
+    return wturns;
+  }
+
+  const start = performance.now();
+  const data = FS.readFileSync("text/day6").toString().split("\n");
+
+  let nodes: Set<string> = new Set<string>();
+  let turns = 1;
+  for (let y = 0; y < LEN; ++y) {
+    for (let x = 0; x < LEN; ++x) {
+      if (isGuard(data[y][x])) {
+        nodes.add(`${y}_${x}`);
+        turns += walk(data[y][x], y, x);
+        break;
+      }
+    }
+  }
+
+  //console.log(`nodes: ${nodes.size}`);
+  const end = performance.now();
+  console.log(`6-1: ${nodes.size} in ${(end - start).toFixed(2)}`);
+}
+
+function day6part2() {
+  const start = performance.now();
+  const LEN = 10;
+  const data = FS.readFileSync("text/d6_test2").toString().split("\n");
+
+  console.log(data);
+
+  const end = performance.now();
+  console.log(`6-2: TODO in ${(end - start).toFixed(2)}`);
 }
